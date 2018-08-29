82d0e884b753f82c5f153914132cecf648ed6a4f
Davisson Paulino <dhpaulino@gmail.com>
2018-08-28 22:09:43 -0300

    created and it's using the variables table

--- src/syntactic.c
+++ src/syntactic.c
@@ -1,12 +1,47 @@
 #include "lex.yy.c"
 #include <regex.h>
 #include <string.h>
+#include <stdlib.h>
+
+typedef struct{
+    char name[999];
+    int value;
+} Variable;
+
+Variable variables[999];
+unsigned int n_var=0;
 
 regex_t DIGIT_REGEX;
 regex_t ID_REGEX;
 regex_t KEYWORD_REGEX;
 regex_t EQUAL_REGEX;
 regex_t PLUS_REGEX;
+
+
+Variable* get_variable(char name[999]){
+    for(int i=0;i<n_var;++i){
+        if(strcmp(variables[i].name, name) == 0){
+            return &variables[i];
+        }
+    }
+    strcpy(variables[n_var].name, name);
+    variables[n_var].value = 0;
+    return &variables[n_var++];
+}
+
+void attribute_variable(char name[999], char value[999]){
+    Variable* var = get_variable(name);
+    var->value = atoi(value);
+}
+
+void add_variable(char name[999], char value[999]){
+    Variable* var = get_variable(name);
+    var->value += atoi(value);
+}
+void print_variable(char name[999]){
+    Variable* var = get_variable(name);
+    printf("%d\n", var->value);
+}
 
 void init_regex(){
     if(regcomp(&DIGIT_REGEX, "^[0-9]", 0) != 0){
@@ -26,28 +61,35 @@ void init_regex(){
         exit(1);
     }
 }
+
+
 int main(){
+    char* first_token; char* sec_token; char* third_token; char* fourth_token;
+    int i=0;
+
 
     init_regex();
     init_lexical();
-    char* token;
-    int i=0;
-    //TODO: create table of variables
+
+    //TODO: handle negative numbers
+    //TODO: print the line where the error occurred
     //TODO: check if ID is not KEYWORD, EQUAL or PLUS
-    while(token = get_next_token()){
+    while(first_token = get_next_token()){
         // printf("%s\n", token);
         printf("token %d:\n", i);
 
-        if(strcmp(token, "let") == 0){
-                token = get_next_token();
-                if(regexec(&ID_REGEX, token, 0, NULL, 0) == 0){
-                    token = get_next_token();
-                    if(regexec(&EQUAL_REGEX, token, 0, NULL, 0) == 0){
-                        token = get_next_token();
-                        if(regexec(&DIGIT_REGEX, token, 0, NULL, 0) == 0){
+        //Attribute
+        if(strcmp(first_token, "let") == 0){
+                sec_token = get_next_token();
+                if(regexec(&ID_REGEX, sec_token, 0, NULL, 0) == 0){
+                    third_token = get_next_token();
+                    if(regexec(&EQUAL_REGEX, third_token, 0, NULL, 0) == 0){
+                        fourth_token = get_next_token();
+                        if(regexec(&DIGIT_REGEX, fourth_token, 0, NULL, 0) == 0){
                             printf("Ok\n");
+                            attribute_variable(sec_token, fourth_token);
                         }else{
-                            printf("erro %s\n", token);
+                            printf("erro %s\n", fourth_token);
                             printf("Error M");
                             exit(1);
                         }
@@ -59,21 +101,22 @@ int main(){
                     printf("Error Y");
                     exit(1);
                 }
-        }else if(strcmp(token, "add") == 0){
-            token = get_next_token();
-            if(regexec(&ID_REGEX, token, 0, NULL, 0) == 0){
-                token = get_next_token();
-                if(regexec(&PLUS_REGEX, token, 0, NULL, 0) == 0){
-                    token = get_next_token();
+        }else if(strcmp(first_token, "add") == 0){
+            sec_token = get_next_token();
+            if(regexec(&ID_REGEX, sec_token, 0, NULL, 0) == 0){
+                third_token = get_next_token();
+                if(regexec(&PLUS_REGEX, third_token, 0, NULL, 0) == 0){
+                    fourth_token = get_next_token();
                     //ID or digit
-                    if(regexec(&ID_REGEX, token, 0, NULL, 0) == 0 || regexec(&DIGIT_REGEX, token, 0, NULL, 0) == 0){
+                    if(regexec(&DIGIT_REGEX, fourth_token, 0, NULL, 0) == 0){
                         printf("Ok\n");
+                        attribute_variable(sec_token, fourth_token);
                     }else{
-                        printf("Error A:  %s", token);
+                        printf("Error A:  %s", fourth_token);
                         exit(1);
                     }
                 }else{
-                    printf("Error B: %s", token);
+                    printf("Error B: %s", third_token);
                     exit(1);
                 }
             }else{
@@ -81,16 +124,17 @@ int main(){
                 exit(1);
             }
 
-        }else if(strcmp(token, "print") == 0){
-            token = get_next_token();
-            if(regexec(&ID_REGEX, token, 0, NULL, 0) == 0){
+        }else if(strcmp(first_token, "print") == 0){
+            sec_token = get_next_token();
+            if(regexec(&ID_REGEX, sec_token, 0, NULL, 0) == 0){
                 printf("ok\n");
+                print_variable(sec_token);
             }else{
                 printf("Error Print");
                 exit(1);
             }
         }else{
-            printf("%s\n", token);
+            printf("%s\n", first_token);
             printf("Error D");
             exit(1);
         }
